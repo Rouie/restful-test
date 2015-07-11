76ac8a63e085675fba3123d81fbfc589fe3594f0
Rovie <rovie16.labutap@gmail.com>
2015-07-11 17:20:48 +0800

    POST : /messages

--- app/routes/messages.php
+++ app/routes/messages.php
@@ -1,37 +1,8 @@
 <?php
 $url = "/messages";
 
-//function doesSenderExists($senderID){
-////    switch(gettype($senderID)){
-////        case "integer":
-//            $query = "SELECT * FROM accounts WHERE accountID=$senderID";
-//            $ret = mysqli_query($link, $query);
-//            $num_rows = mysqli_num_rows($ret);
-//
-//            if($num_rows <= 0)
-//                return false;
-//
-//            return true;
-////        break;
-////        
-////        case "string":
-////            Response::BadRequest("Sender is invalid.");
-////        break;
-////    }
-//}
-//
-//function doesReceiverExists($receiverID){
-//    $query = "SELECT * FROM accounts WHERE accountID=$receiverID";
-//    $ret = mysqli_query($link, $query);
-//    $num_rows = mysqli_num_rows($ret);
-//    
-//    if($num_rows <= 0)
-//        return false;
-//        
-//    return true;
-//}
-
 function doesAccountExists($accountID){
+    echo intval($accountID);
     $query = "SELECT * FROM accounts WHERE accountID=$accountID";
     $ret = mysqli_query($link, $query);
     $num_rows = mysqli_num_rows($ret);
@@ -43,16 +14,21 @@ function doesAccountExists($accountID){
 }
 
 $app->post($url, function() use($link){
-    if(doesAccountExists($_POST["senderID"]))
-    if(doesAccountExists($_POST["receiverID"])){
-        $query = "INSERT INTO messages(`senderID`, `receiverID`, `message`, `dateTime`) VALUES({$_POST["senderID"]}, {$_POST["receiverID"]}, '{$_POST["message"]}', NOW())";
-        $ret = mysqli_query($link, $query);
-
-        if($ret){
-            echo $query;
-        }
-    }else{
+    if(!doesAccountExists($_POST["senderID"]))
+        Response::NotFound("Sender does not exists.");
+    if(!doesAccountExists($_POST["receiverID"]))
         Response::NotFound("Receiver does not exists.");
+    
+    $query = "INSERT INTO messages(`senderID`, `receiverID`, `message`, `dateTime`) VALUES({$_POST["senderID"]}, {$_POST["receiverID"]}, '{$_POST["message"]}', NOW())";
+    $ret = mysqli_query($link, $query);
+
+    if($ret){
+        Response::Created("Account was successfully created.");
     }
+});
+
+$app->get($url, function() use($link){
+    $query = "SELECT * FROM messages";
+    $ret = mysqli_query($link, $query);
 });
 ?>
