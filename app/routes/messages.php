68504641f6052fe1ba22e61d9a100eefa46b0410
Rovie <rovie16.labutap@gmail.com>
2015-07-11 19:16:06 +0800

    Added Resources
    Added GET /messages
    Added GET /messages/id
    Added DELETE /messages
    Added DELETE /messages/id

--- app/routes/messages.php
+++ app/routes/messages.php
@@ -23,7 +23,7 @@ $app->post($url, function() use($link){
     $ret = mysqli_query($link, $query);
 
     if($ret){
-        Response::Created("Account was successfully created.");
+        Response::Created("Message was successfully created.");
     }
 });
 
@@ -41,12 +41,12 @@ $app->get($url, function() use($link){
     }
 });
 
-$app->get($url . "/:messageID", function($messageID) use($link){
+$app->get($url . "/:id", function($messageID) use($link){
     $query = "SELECT * FROM messages WHERE messageID=$messageID";
     $ret = mysqli_query($link, $query);
     $result = mysqli_fetch_assoc($ret);
     
-    if(mysqli_num_rows <= 0)
+    if(mysqli_num_rows($ret) <= 0)
         Response::NotFound("Message with ID $messageID does not exists.");
     
     $return = [
@@ -58,5 +58,24 @@ $app->get($url . "/:messageID", function($messageID) use($link){
     ];
     
     Response::Ok($return, JSON_PRETTY_PRINT);
+});
+
+$app->delete($url, function() use($link){
+    $query = "DELETE FROM messages";
+    $ret = mysqli_query($link, $query);
+    
+    if($ret){
+        Response::Ok("Messages were successfully deleted.");
+    }
+});
+
+$app->delete($url . "/:id", function($messageID) use($link){
+    $query = "DELETE FROM messages WHERE messageID=$messageID";
+    $ret = mysqli_query($link, $query);
+    
+    if(mysqli_num_rows($ret) <= 0)
+        Response::NotFound("Message with ID $messageID does not exists.");
+    
+    Response::Ok("Message was successfully deleted.");
 });
 ?>
